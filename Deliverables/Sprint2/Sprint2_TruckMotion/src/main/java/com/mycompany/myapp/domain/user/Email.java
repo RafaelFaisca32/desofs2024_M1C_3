91f66eddca1be5a476ca7dc56e0371a31cabd078
Rafael Faísca <1180658@isep.ipp.pt>
2024-06-03 19:08:42 +0100

    #9 User Value Objects

---
+++ Deliverables/Sprint2/Sprint2_TruckMotion/src/main/java/com/mycompany/myapp/domain/user/Email.java
@@ -0,0 +1,39 @@
+package com.mycompany.myapp.domain.user;
+
+import java.util.Objects;
+
+public class Email {
+
+    private String email;
+
+    public Email(String email) {
+        this.email = email;
+    }
+
+    //JPA
+    public Email() {
+        this.email = "";
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Email email1 = (Email) o;
+        return Objects.equals(email, email1.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(email);
+    }
+
+    @Override
+    public String toString() {
+        return email;
+    }
+}
---
+++ Deliverables/Sprint2/Sprint2_TruckMotion/src/main/java/com/mycompany/myapp/domain/user/ResetDate.java
@@ -0,0 +1,41 @@
+package com.mycompany.myapp.domain.user;
+
+import java.time.Instant;
+import java.util.Objects;
+
+public class ResetDate {
+
+    private Instant resetDate;
+
+    public ResetDate(Instant resetDate) {
+        this.resetDate = resetDate;
+    }
+
+
+    //JPA
+    public ResetDate() {
+        this.resetDate = null;
+    }
+
+    public Instant getResetDate() {
+        return resetDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ResetDate resetDate1 = (ResetDate) o;
+        return Objects.equals(resetDate, resetDate1.resetDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(resetDate);
+    }
+
+    @Override
+    public String toString() {
+        return resetDate.toString();
+    }
+}
